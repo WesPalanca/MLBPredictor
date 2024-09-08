22f6a67d909e0b442c63f29880774a06c6c5b8c7
WesPalanca <wespalanca@gmail.com>
2024-09-07 22:28:03 -0400

    jwt authentication and cors configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,14 +2,19 @@ from flask import Flask
 from .config import Config
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
 
 db = SQLAlchemy()
+jwt = JWTManager()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  
     db.init_app(app)
-    CORS(app)
+    jwt.init_app(app)
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
     from .routes import register_blueprints
     with app.app_context(): # create tables
         db.create_all()
